4c41f5f5a148d7f0d6a977fdd73f1822b87387f9
Yonathan Jaramillo <yonathanmlb@hotmail.com>
2026-04-04 22:32:07 -0400

    Update positiveSum function with task details
    Refactor positiveSum function to include task description and example.

--- sum-of-positives.js
+++ sum-of-positives.js
@@ -1,3 +1,27 @@
+Task
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example
+[1, -4, 7, 12] => 
+1
++
+7
++
+12
+=
+20
+1+7+12=20
+Note
+If there is nothing to sum, the sum is default to 0
+
+
+
+
+Solution:
+
+
+
+
 function positiveSum(arr) {
   let sum = 0;
   
